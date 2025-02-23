78a6f07959c914f1e65d8921518abd70f0e3197f
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-02-23 23:40:05 +0300

    feat: added checkbox to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,19 +10,27 @@ const Card: FC<CardProps> = ({ anime, onAnimeSelect }) => {
   return (
     <div
       role="article"
-      onClick={() => onAnimeSelect(anime)}
       className="card hover:cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 p-4 bg-white shadow-md rounded-lg w-full flex"
     >
       <img
         src={anime.images?.webp?.image_url || ''}
         alt={anime.title}
         className="w-32 h-32 object-cover rounded-lg mr-4"
+        onClick={() => onAnimeSelect(anime)}
       />
 
       <div>
-        <h3 className="text-xl font-semibold text-gray-800 mb-2">
-          {anime.title}
-        </h3>
+        <div className="flex justify-between items-center">
+          <h3
+            onClick={() => onAnimeSelect(anime)}
+            className="text-xl font-semibold text-gray-800 mb-2"
+          >
+            {anime.title}
+          </h3>
+          <div>
+            <input type="checkbox" />
+          </div>
+        </div>
         <p className="text-gray-600 text-sm hover:cursor-text">
           {anime.synopsis
             ? anime.synopsis.substring(0, 100) + '...'
